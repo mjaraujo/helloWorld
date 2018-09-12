70675eba4bf6670ac0339e6feac355d413a86bdf
a1838075 <a1838075@10.0.56.22>
2018-09-12 11:25:30 -0300

    Autor: Nathalie Martins Criação da Classe App Android Vendas

---
+++ HelloWorld/src/helloworld/mensagens/AppAndroidVendas.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld.mensagens;
+
+import helloworld.Mensagem;
+
+/**
+ *
+ * @author a1838075
+ */
+public class AppAndroidVendas extends Mensagem {
+    
+    @Override
+    protected void exibirMensagem() {
+        System.out.println("Classe App Android Vendas Implementada por Nathalie Martins");
+    }
+    
+}
